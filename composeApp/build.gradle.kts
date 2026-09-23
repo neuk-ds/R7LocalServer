b1b27ded4d3acb9a725a37598e93c984b10f7399
mr_nds <nds151092@gmail.com>
2026-09-23 19:50:27 +0300

    release

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -1,6 +1,6 @@
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 
-val applicationVersion = "2.4.1"
+val applicationVersion = "2.4.2"
 val generatedBuildInfoDir = layout.buildDirectory.dir("generated/build-info/kotlin")
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
